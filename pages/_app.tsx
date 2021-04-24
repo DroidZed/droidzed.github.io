f607d8c552e3ad576bd4a4253fbafaf255a686a7
DroidZed <aymen.dragon5@gmail.com>
2021-04-24 22:59:23 +0100

    initial step

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import '../styles/globals.css'
+
+function App({ Component, pageProps }: { Component: any, pageProps: any }): JSX.Element {
+  return <Component {...pageProps} />
+}
+
+export default App

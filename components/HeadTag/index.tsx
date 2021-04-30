b791c3994264f53b1b3105069970e5695f21300a
DroidZed <aymen.dragon5@gmail.com>
2021-04-30 09:28:01 +0100

    added meta tags

--- components/HeadTag/index.tsx
+++ components/HeadTag/index.tsx
@@ -13,10 +13,23 @@ const HeadTag: React.FC<Props> = (props) => {
 				name='viewport'
 				content='width=device-width, initial-scale=1, shrink-to-fit=no'
 			/>
-			<meta name='author' content='Aymen Dhahri' />
+			<meta
+				name='google-site-verification'
+				content='J5tfFdlGdM5ApvAHEsMg-CBPSyJv6wHdEtP45wMxEPU'
+			/>
+			<meta name='twitter:card' content='summary' />
+			<meta name='twitter:site' content='@Droid7ed' />
+			<meta name='twitter:creator' content='@Droid7ed' />
+			<meta property='og:url' content='https://droidzed.me' />
+			<meta property='og:title' content='Personal Website' />
+			<meta
+				property='og:description'
+				content='Sanctuary of DroidZed, my personal website and portfolio. Come check it out and discover more about my world.'
+			/>
+			<meta property='og:image' content='https://www.droidzed.me/favicon.png' />
 			<meta name='description' content='Personal website' />
 			<meta name='keywords' content='website,personal' />
-			<meta charSet='utf-8' />
+			<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 			<link rel='icon' href='/favicon.png' />
 		</Head>
 	);

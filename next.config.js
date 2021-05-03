962a821537b2623dec800c188d27871d9c4a137f
Aymen Dhahri <aymen.dragon5@gmail.com>
2021-05-03 15:32:17 +0100

    upgraded webpack version to v5

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
+
+	future: {
+		webpack5: true
+	},
 	images: {
 		domains: ["avatars.githubusercontent.com"],
 	},
@@ -15,7 +19,7 @@ module.exports = withPWA({
 	pwa: {
 		disable: process.env.NODE_ENV === 'development',
 		register: true,
-		scope: '/app',
+		scope: '/',
 		sw: 'service-worker.js',
 		dest: "public",
 		runtimeCaching,

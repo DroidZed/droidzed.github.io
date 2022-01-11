08a9497c6ebddf6f7e7fa70878aae66b91393c08
DroidZed <aymen.dragon5@gmail.com>
2022-01-11 20:15:08 +0100

    REVERTED BACK, this is useless 🤦🏻‍♂️

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
-const dotenv = require("dotenv")
-
-dotenv.config()
-
-const dev = process.env.NODE_ENV !== 'production';
-console.log(process.env.NODE_ENV);
 module.exports = {
-	content: dev ? [] : ['./**/*.html'],
+	content: ['./**/*.html'],
 	theme: {
 		extend: {
 			fontFamily: {
